e91fd51d7b5639ee6f324c730154cd5148baac46
Andreas Holmstedt <holmstedt.andreas@hotmail.com>
2018-06-11 11:53:45 +0200

    created report and added some tests

--- src/canGetLicense.test.js
+++ src/canGetLicense.test.js
@@ -2,14 +2,24 @@ import {canGetLicense} from './canGetLicense';
 
 
 describe("Drivers license check test suite", ()=> {
-  test("...", () => {
+  test("Test A driving license", () => {
+    expect(canGetLicense("A", 23)).toBe(false)
+    expect(canGetLicense("A", 35)).toBe(true)
+  })
+  test("Test B driving license", () => {
     expect(canGetLicense("B", 18)).toBe(true)
-  })
-  test("...", () => {
+    expect(canGetLicense("B", 16)).toBe(false)
     expect(canGetLicense("b", 18)).toBe(true)
+    expect(canGetLicense("BE", 24)).toBe(true)
+    expect(canGetLicense("BE", 17)).toBe(false)
   })
-  test("...", () => {
+  test("Test C driving license", () => {
     expect(canGetLicense("C", 32)).toBe(true)
+    expect(canGetLicense("C", 19)).toBe(false)
+  })
+  test("Test D driving license", () => {
+    expect(canGetLicense("D", 24)).toBe(true)
+    expect(canGetLicense("D", 21)).toBe(false)
   })
   test("Should throw an error when trying to get a license when age is not a number", () => {
     expect( () => { canGetLicense("g", "hej") }).toThrow()
